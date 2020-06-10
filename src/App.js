74be3e1e2ad3babaf3c9232513ecc25d2a753e63
Your Name <you@example.com>
2020-06-10 19:33:22 +0700

    10/06

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import axios from "axios";
-import Swal from 'sweetalert2'
-import { ToastContainer, toast } from 'react-toastify';
+import Swal from "sweetalert2";
+import { ToastContainer, toast } from "react-toastify";
 
 import { Container, Row, Col } from "reactstrap";
 import FormTable from "./Component/Table";
 import Form from "./Component/Form";
 import Pagination from "./Component/Pagination";
-import ControlForm from './Component/ControlForm'
+import ControlForm from "./Component/ControlForm";
 import "./App.css";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import { useState, useEffect } from "react";
 
 function App() {
   const [products, setProducts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [productPerPage,setProductPerPage] = useState(5);
-  const [sortBy,setSortBy]=useState('name');
-  const [sortValue,setSortValue]=useState(1)
+  const [productPerPage, setProductPerPage] = useState(5);
+  const [sortBy, setSortBy] = useState("name");
+  const [sortValue, setSortValue] = useState(1);
 
   const [showForm, setShowForm] = useState(false);
 
@@ -36,34 +36,32 @@ function App() {
       })
       .then((res) => setProducts(products.concat(res.data)))
       .catch((error) => console.error(error));
-      addSucc();
+    addSucc();
   };
 
   const handleDelete = (id) => {
     Swal.fire({
-      title: 'Are you sure?',
-      text: 'Product will be delete!',
-      icon: 'warning',
+      title: "Are you sure?",
+      text: "Product will be delete!",
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes!'
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes!",
     }).then((result) => {
       if (result.value) {
         axios
           .delete(`${baseUrl}/${id}`)
 
           .then(() => {
-            // setProducts(products.filter(product=>product.id!==id))
             let filterList = products.filter((product) => product.id !== id);
             setProducts(filterList);
           })
           .catch((error) => console.error(error));
-        // setProducts(products.filter(product=>product.id!==id))
 
         deleteSucc();
       }
-    })
+    });
   };
 
   const getDataToPut = (nameValue, priceValue, stockValue, id) => {
@@ -79,75 +77,72 @@ function App() {
         )
       )
       .catch((error) => console.error(error));
-      updateSucc();
+    updateSucc();
   };
 
   const indexOfLastProduct = currentPage * productPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productPerPage;
-  const currentProduct = products.slice(indexOfFirstProduct,indexOfLastProduct);
+  const currentProduct = products.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
 
   const paginate = (number) => {
     setCurrentPage(number);
   };
 
-//toast
-  const addSucc = () => toast("Added successfully!"); 
+  //toast
+  const addSucc = () => toast("Added successfully!");
   const deleteSucc = () => toast("Deleted successfully!");
   const updateSucc = () => toast("Updated successfully!");
 
-  const handleSearch = keyword => {
-    if (keyword.length>0) {
-      const result= products.filter(product=>{
-        return product.name.toLowerCase().includes(keyword.toLowerCase())
+  const handleSearch = (keyword) => {
+    if (keyword.length > 0) {
+      const result = products.filter((product) => {
+        return product.name.toLowerCase().includes(keyword.toLowerCase());
       });
-      setProducts(result)
-     }
-
+      setProducts(result);
+    }
 
     // else
     // {
     //   return products;
     // }
+  };
 
-  }
+  const onSort = (by, value) => {
+    setSortBy(by);
+    setSortValue(value);
 
-  const onSort=(by,value)=>{
-   setSortBy(by);
-   setSortValue(value);
-  //  console.log(sortBy,sortValue);
-  console.log(`${by} - ${value}`);
+    if (by === "name") {
+      products.sort((a, b) => {
+        if (a.name > b.name) return value;
+        else if (a.name < b.name) return -value;
+        else return 0;
+      });
+    } else if (by === "price") {
+      products.sort((a, b) => {
+        if (a.price > b.price) return -value;
+        else if (a.price < b.price) return value;
+        else return 0;
+      });
+    } else {
+      products.sort((a, b) => {
+        if (a.stock > b.stock) return -value;
+        else if (a.stock < b.stock) return value;
+        else return 0;
+      });
+    }
+  };
+
+  const onShowForm = (formStatus) => {
+    setShowForm(formStatus);
+  };
+
+  const onChangeProductPerPage = (numberProduct) => {
+    setProductPerPage(numberProduct);
+  };
   
-    if(by==='name'){
-      products.sort((a,b)=>{
-        if(a.name>b.name) return value;
-        else if(a.name<b.name) return -value;
-        else return 0;
-      })
-    }
-    else if(by==='price'){
-      products.sort((a,b)=>{
-        if(a.price>b.price) return -value;
-        else if(a.price<b.price) return value;
-        else return 0;
-      })
-    }
-    else{
-      products.sort((a,b)=>{
-        if(a.stock>b.stock) return -value;
-        else if(a.stock<b.stock) return value;
-        else return 0;
-      })
-    }
-  }
-
-  const onShowForm =(formStatus)=>{
-      setShowForm(formStatus);
-  }
-  const onChangeProductPerPage =(numberProduct)=>{
-    setProductPerPage(numberProduct)
-    
-    
-  }
   return (
     <>
       <Container>
@@ -156,7 +151,12 @@ function App() {
         </div>
 
         <Row className="control">
-          <ControlForm handleSearch={handleSearch} onSort={onSort} onShowForm={onShowForm} onChangeProductPerPage={onChangeProductPerPage}/>
+          <ControlForm
+            handleSearch={handleSearch}
+            onSort={onSort}
+            onShowForm={onShowForm}
+            onChangeProductPerPage={onChangeProductPerPage}
+          />
         </Row>
 
         <Row>
@@ -177,7 +177,6 @@ function App() {
               productPerPage={productPerPage}
               totalProduct={products.length}
               paginate={paginate}
-            
             />
           </Col>
         </Row>
